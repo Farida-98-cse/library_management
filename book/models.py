167c1aa107eae2b5f3adc16a845ed4b86e885e24
f.mehrpour <mehrpourfarida@gmail.com>
2023-05-25 14:49:26 +0330

    feat(Book-model): define book class

--- book/models.py
+++ book/models.py
@@ -5,3 +5,13 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
+    author = models.CharField(max_length=30, blank=True, null=True)
+    borrow_inventory = models.IntegerField(default=5)
+    sell_inventory = models.IntegerField(default=5)
+    sell_price = models.FloatField(default=10.0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"])
+        ]
+
--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 from book.views.book import (
     retrieve_book
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("retrieve-book", retrieve_book)
 ]
