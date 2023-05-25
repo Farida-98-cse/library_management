46e927faf9d3191010e52d5500e14a656a3f559f
f.mehrpour <mehrpourfarida@gmail.com>
2023-05-25 17:54:21 +0330

    fix(account model)

--- book/models.py
+++ book/models.py
@@ -54,22 +54,22 @@ class Category(models.Model):
         ]
 
 
-class Account(models.Model):
-    inventory = models.FloatField(default=20.0)
-
-
 class CustomUser(AbstractBaseUser):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     national_id = models.CharField(max_length=20, blank=True, default="")
-    account = models.OneToOneField(
-        Account,
-        on_delete=models.CASCADE,
-        null=True
-    )
     role = models.CharField(max_length=10, choices=[(tag, tag.value) for tag in Role], default=Role.member)
 
     def __str__(self):
         return self.national_id
+
+
+class Account(models.Model):
+    inventory = models.FloatField(default=20.0)
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        null=True
+    )
 
 
 class Order(models.Model):
