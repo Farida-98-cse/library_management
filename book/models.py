a8e00df5bf4b5b52f2ecb527ba9246b1c689e4fd
f.mehrpour <mehrpourfarida@gmail.com>
2023-05-25 17:38:14 +0330

    feat(Order model)

--- book/models.py
+++ book/models.py
@@ -4,9 +4,19 @@ from django.contrib.auth.models import AbstractUser, User
 from enum import Enum
 
 
-class Role(str, Enum):
+class Role(Enum):
     staff = 'Staff'
     member = 'Member'
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
+
+
+class BookStatus(Enum):
+    checkout = 'CheckOut'
+    hold = 'Hold'
+    sold = 'Soled'
 
     @classmethod
     def choices(cls):
@@ -47,6 +57,18 @@ class Category(models.Model):
 class CustomUser(AbstractBaseUser):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     national_id = models.CharField(max_length=20, blank=True, default="")
+    role = models.CharField(max_length=10, choices=[(tag, tag.value) for tag in Role], default=Role.member)
 
     def __str__(self):
         return self.national_id
+
+
+class Order(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    is_returned = models.BooleanField()
+    return_date = models.DateField(null=True)
+    status = models.CharField(max_length=10, choices=[(tag, tag.value) for tag in BookStatus],
+                              default=BookStatus.sold)
